a600dec73a625df9b59a6875b5b15419e8fa4631
Stéphane Roucheray <stephane.roucheray@perfect-memory.com>
2023-05-05 15:42:40 +0200

    chore: fixing configuration for tests

--- apps/vscode-webview/jest.config.ts
+++ apps/vscode-webview/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'vscode-webview',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../coverage/vscode-webview',
   transform: {
